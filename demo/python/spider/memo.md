aabf0ca533b80ee7a1d0b472ea98035fc9c327f2
w <heaven@cme.com>
2021-08-10 23:13:31 +0800

    py get stm first page

---
+++ demo/python/spider/memo.md
@@ -0,0 +1,26 @@
+1. 环境
+
+1.1 python安装
+
+1.2 请求库
+```
+pip3 install requests
+pip3 install wheel
+pip3 install requests-2.17.3-py2.py3-none-any.whl
+pip3 install selenium
+pip3 install aiohttp
+pip3 install cchardet aiodns
+
+# 
+``` 
+下载 [PhantomJS](http://phantomjs.org/download.html)并配置环境变量
+
+1.3 解析库的安装
+```
+pip3 install lxml
+pip3 install pyquery
+```
+tesseract 下载地址：http://digi.bib.uni-mannheim.de/tesseract
+```
+pip3 install tesserocr pillow
+```
--- demo/python/spider/spider.py
+++ demo/python/spider/spider.py
@@ -1,17 +1,54 @@
-from urllib.request import urlopen
+# from urllib.request import urlopen
+#from urllib import request
+import urllib.request
+import urllib.parse
+import http.cookiejar
+import os, time
 from bs4 import BeautifulSoup as bf
-from urllib.request import urlretrieve
+from urllib.request import Request, urlretrieve
+from selenium import webdriver
 
-html = urlopen("https://store.steampowered.com/vr/")
-# html_text = bytes.decode(html.read())
-html_bf = bf(html.read(), 'html.parser')
+# construct request
+url = 'https://store.steampowered.com/vr/'
+headers = {
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+    'Host': 'store.steampowered.com'
+}
+req = Request(url=url, headers=headers, method='GET')
+
+# send request
+# resp = urllib.request.urlopen(req)
+cookie = http.cookiejar.CookieJar()
+handler = urllib.request.HTTPCookieProcessor(cookie)
+html_opener = urllib.request.build_opener(handler)
+resp = html_opener.open(req)
+html = resp.read()
+html_bf = bf(html, 'html.parser')
 html_title = html_bf.head.title
 
+# print dialog
+print(resp.getheaders())
 print(html_title)
+
+# save cookie
+html_cookie = {}
+for i in cookie:
+    html_cookie[i.name] = i.value
+    print(i.name, i.value)
+
+
+# make diretory
+time_str = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+path_dir = './tmp/' + time_str + '/'
+os.mkdir(path_dir)
+# download feature pics
 game_pic = html_bf.find_all('img', class_="tab_item_cap_img")
 count = 0
-
 for i in game_pic:
     print(i['src'])
     count += 1;
-    urlretrieve(i['src'], './tmp/'+str(count)+'.jpg')
+    urlretrieve(i['src'], path_dir+str(count)+'.jpg')
+print(str(count)+' pics'+ ' stored into' + path_dir)
+
+# start chrome
+browser = webdriver.Chrome()
