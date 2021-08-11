3c940474a7331436fcc1288c9b49f8a194e26a8b
w <heaven@cme.com>
2021-08-12 00:16:41 +0800

    browser scroll

--- demo/python/spider/spider_scroll.py
+++ demo/python/spider/spider_scroll.py
@@ -13,44 +13,43 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-# construct request
-url = 'https://store.steampowered.com/vr/'
-headers = {
-    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-    'Host': 'store.steampowered.com'
-}
-req = Request(url=url, headers=headers, method='GET')
 
-# send request
-# resp = urllib.request.urlopen(req)
-cookie = http.cookiejar.CookieJar()
-handler = urllib.request.HTTPCookieProcessor(cookie)
-html_opener = urllib.request.build_opener(handler)
-resp = html_opener.open(req)
-html = resp.read()
-html_bf = bf(html, 'html.parser')
-html_title = html_bf.head.title
+vr_url = 'https://store.steampowered.com/search/?sort_by=Released_DESC&tags=-1&vrsupport=402'
+page_scroll_n = 3
+game_ret_box = 'a'
+game_ret_class = 'search_result_row'
+game_ret_name_box = 'span'
+game_ret_name_class = 'title'
 
-# print dialog
-print(resp.getheaders())
-print(html_title)
-
-# save cookie
-html_cookie = {}
-for i in cookie:
-    html_cookie[i.name] = i.value
-    print(i.name, i.value)
+# game info
+game_info = {}
+game_all = []
 
 # start chrome
 browser = webdriver.Chrome()
-vr_url = 'https://store.steampowered.com/search/?sort_by=Released_DESC&tags=-1&vrsupport=402'
 browser.get(vr_url)
-page_scroll_n = 10
+
 while page_scroll_n > 0:
+    time.sleep(1)
     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     time.sleep(1)
     page_scroll_n = page_scroll_n - 1
 
+# resp = urllib.request.urlopen(req)
+html = browser.page_source
+html_bf = bf(html, 'html.parser')
+html_title = html_bf.head.title
+
+# print dialog
+print(html_title)
+count = 0
+game_box = html_bf.find_all(game_ret_box, class_=game_ret_class)
+for i in game_box:
+    print(i['href'])
+    count += 1;
+    game_info['href'] = i['href']
+    game_info['data-ds-itemkey'] = i['data-ds-itemkey']
+    game_all.append(game_info)
 
 # make diretory
 time_str = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
@@ -64,4 +63,7 @@ for i in game_pic:
     count += 1;
     urlretrieve(i['src'], path_dir+str(count)+'.jpg')
 print(str(count)+' pics'+ ' stored into' + path_dir)
+
+# close browser
+browser.close()
 
