c000a64edd978956090d6673140096d50645e550
w <heaven@cme.com>
2021-08-16 00:23:42 +0800

    get html children ok

--- demo/python/spider/spider_scroll.py
+++ demo/python/spider/spider_scroll.py
@@ -18,6 +18,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 #cur_path = os.environ['PATH']
 #os.environ['PATH'] = cur_path + ';' +  
 
+# func
+def getValByClass(pattern, source, *result):
+    result = source.find_all(class_=re.compile(pattern))
+    if len(result) == 0:
+        print("error: ",__file__,sys._getframe().f_lineno)
+    return len(result)
+
 vr_url = 'https://store.steampowered.com/search/?sort_by=Released_DESC&tags=-1&vrsupport=402'
 page_scroll_n = 3
 game_ret_box = 'a'
@@ -32,7 +39,9 @@ game_all = []
 
 
 # start browser
-browser = webdriver.Edge(executable_path='./env/msedgedriver.exe')
+driver_url = './env/msedgedriver.exe'
+os.environ["webdriver.Edge.driver"] = driver_url
+browser = webdriver.Edge(executable_path=driver_url)
 browser.get(vr_url)
 
 while page_scroll_n > 0:
@@ -53,6 +62,7 @@ count = 0
 game_basic = []
 game_id = 0
 game_box = html_bf.find_all(game_ret_box, class_=game_ret_class)
+game_de_sr = []
 print('total game: ' + str(len(game_box)))
 for i in game_box:
     count += 1;
@@ -61,15 +71,34 @@ for i in game_box:
     print('No.' + str(count) + ': ')
     print('---- ' + i['href'])
     print('len: ' + str(len(game_basic)))
+    # href not match, break
+    if len(game_basic) == 0:
+        break
     
-    if len(game_basic) > 0:
-        print(game_basic[0][0])
-        print(game_basic[0][1])
-        game_info['id'] = game_basic[0][0]
-        game_info['name'] = game_basic[0][1]
-        game_info['href'] = i['href']
-        game_all.append(game_info)
+    # get basic info from href
+    game_info['id'] = game_basic[0][0]
+    game_info['name_abbr'] = game_basic[0][1]
+    game_info['href'] = i['href']
+    # get more info from children
+    # get name
+    game_de_sr = i.find_all(class_=re.compile("title"))
+    if len(game_de_sr) == 0:
+        print("error: ",__file__,sys._getframe().f_lineno)
+        break
+    game_info['name'] = str(game_de_sr[0].text)
 
+    # log
+    print('id: ' + game_info['id'])
+    print('name_abbr: ' + game_info['name_abbr'])
+    print('name: ' + game_info['name'])
+    
+
+
+
+
+    # append info node to list
+    game_all.append(game_info)
+ 
 '''
 # make diretory
 time_str = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
